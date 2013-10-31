42b67b43973fd0b75a20219cc14d970fa2a88a12
vagrant <vagrant@rails-dev-box>
2013-10-31 11:21:14 +0000

    First commit with modified Readme file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = 'bd4672d662be751950e275c59d192b18d60ea78f565c6d3ab4aab5d6b43275a47382e63738692173185b08530615c7e75fb836614b95cf1bf6911424254c7a1d'
