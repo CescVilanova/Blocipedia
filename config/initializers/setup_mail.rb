0e71ccaea3cc125fec59e4812854938eb64ffaf1
Cesc Vilanova <vfrancesc@gmail.com>
2013-11-07 18:58:10 +0100

    Trying to solve signing up on production.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 if Rails.env.development?
-  ActionMailer::Base.delivery_method = :sendmail
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => "app19226463@heroku.com",
-    :password       => "s4qoxhv2",
+    :user_name      => "app19226463@heroku.com", 
+    :password       => "s4qoxhv2", 
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
